3df36a7a29f382ef694b98f6c0dbb4160323084b
Kirill <kirill.petrov.centrale@gmail.com>
2020-07-07 13:54:49 +0200

    fill recipes on click

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import recettes from './recettes'
 
 import Header from './components/Header'
 import Admin from './components/Admin'
-
+import Card from './components/Card'
 class App extends Component {
   state = {
     pseudo: this.props.match.params.pseudo,
@@ -14,13 +14,13 @@ class App extends Component {
 loadExemple = () => this.setState({ recettes })
 
   render () {
+    const cards = Object.keys(this.state.recettes)
+      .map(key => <Card key={key} details={this.state.recettes[key]}/>)
     return (
       <div className='box'>
         <Header pseudo={this.state.pseudo}/> 
         <div className='cards'>
-          <div className='card'>
-            <h2>Une Carte</h2>
-          </div>
+          { cards }
         </div>
         <Admin 
           loadExemple={this.loadExemple}
