3257eeba96abb17f371366f75c6de11935379fde
Kirill <kirill.petrov.centrale@gmail.com>
2020-07-07 11:32:51 +0200

    init project and add Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = ({ pseudo }) => {
+    return (
+        <header>
+            <h1>La boite à recette de {pseudo}</h1>
+        </header>
+    )
+}
+
+export default Header
