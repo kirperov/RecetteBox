93e1ba47a24445205251adfb224d5ef180640b37
Kirill <kirill.petrov.centrale@gmail.com>
2020-07-07 14:43:38 +0200

    show images and formated li an ol lists generated with split()

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
 const Card = ({details}) => {
+    const ingredients = details.ingredients
+        .split(',')
+        .map(item => <li key={item}>{item}</li>)
+        const instructions = details.instructions
+        .split('\n')
+        .map(item => <li key={item}>{item}</li>)
     return (
         <div className="card">
+            <div className="image">
+                <img src={require(`../img/${details.image}`)} alt={details.nom}></img>
+            </div>
             <div className="recette">
                 <h2>{details.nom}</h2>
+                <ul className="list-ingredients">
+                    {ingredients}
+                </ul>
+                <ol className="list-instructions">
+                    {instructions}
+                </ol>
             </div>
         </div>
     );
