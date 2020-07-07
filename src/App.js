82e8ec246ef15f15fed3060d47d9340dfd551ddb
Kirill <kirill.petrov.centrale@gmail.com>
2020-07-07 16:22:41 +0200

    connexion with firebase

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React, { Component } from 'react'
 // CSS
 import './App.css'
 import recettes from './recettes'
-
 import Header from './components/Header'
 import Admin from './components/Admin'
 import Card from './components/Card'
+// Firebase 
+import base from './base'
 class App extends Component {
   state = {
     pseudo: this.props.match.params.pseudo,
     recettes: {}
   }
+
+  //Synchronise Database firebase
+  componentDidMount() {
+      this.ref =  base.syncState(`/${this.state.pseudo}/recettes`, {
+      context: this,
+      state: 'recettes'
+    })
+  }
+
+  //End synchronisation with base firebase when change user
+  componentWillUnmount() {
+    base.removeBinding(this.ref)
+  }
+
 loadExemple = () => this.setState({ recettes })
 
   render () {
--- src/base.js
+++ src/base.js
@@ -3,9 +3,9 @@ import firebase from 'firebase/app'
 import 'firebase/database'
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'xxx',
-  authDomain: 'xxx',
-  databaseURL: 'xxx'
+  apiKey: "AIzaSyCIUt-VVRYl410JTpqo4UWQu_BRTVeYOPk",
+  authDomain: "recettebox-6fc70.firebaseapp.com",
+  databaseURL: "https://recettebox-6fc70.firebaseio.com",
 })
 
 const base = Rebase.createClass(firebaseApp.database())
